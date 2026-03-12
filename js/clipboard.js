b3b661a598f29923ebe78eb91d8b9106f8805c3e
EvgenyMak <evgenyzheka282662@gmaul.com>
2026-03-12 23:37:30 +0700

    sss

---
+++ js/clipboard.js
@@ -0,0 +1,15 @@
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text);
+
+  const plate = document.querySelector('.copy__box');
+  plate.classList.add('show');
+
+  setTimeout(() => {
+    plate.classList.remove('show');
+  }, 2000);
+}
+
+function copyNumberToClipboard(element) {
+  const result = element.innerText.split(" ").join("").slice(1);
+  copyToClipboard(result);
+}
