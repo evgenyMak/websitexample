d31fe8bedc968579bbb6b4e3ebeb7c9acdfd6dd6
EvgenyMak <evgenyzheka282662@gmaul.com>
2026-03-13 19:49:55 +0700

    аа2

--- js/alert.js
+++ js/alert.js
@@ -1,6 +1,6 @@
 export default function alertText(text) {
-  const plate = document.querySelector(".copy__box");
-  plate.getElementsByClassName("copy__box-text")[0].innerText = text;
+  const plate = document.querySelector(".alert__box");
+  plate.getElementsByClassName("alert__box-text")[0].innerText = text;
   plate.classList.add("show");
 
   setTimeout(() => {
