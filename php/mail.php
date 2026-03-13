e6fa5d3afab61ac7313a342d838908093dcb4a83
EvgenyMak <evgenyzheka282662@gmaul.com>
2026-03-13 16:34:33 +0700

    fix php

---
+++ php/mail.php
@@ -0,0 +1,9 @@
+<?php
+
+$headers  = "Content-type: text/html; charset=windows-1251 \r\n"; 
+$headers .= "From: От кого письмо <from@example.com>\r\n"; 
+$headers .= "Reply-To: reply-to@example.com\r\n"; 
+
+mail("e04561029@gmail.com", "Test Header", "Test Message")
+
+    ?>
