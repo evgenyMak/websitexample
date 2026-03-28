b06cfb96dcb26bac0c9db1be6271abf290c96325
EvgenyMak <evgenyzheka282662@gmaul.com>
2026-03-28 12:52:42 +0700

    чч

--- js/mail.js
+++ js/mail.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("#form");
-const scriptUrl = "https://script.google.com";
+const scriptUrl = "https://script.google.com/macros/s/AKfycbxjk_y12et_RaASCWDdGuSYmcATC4ih5sB9w6WGk8qGjaH27ALfUFoqjgyae0SEVeYARw/exec";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
