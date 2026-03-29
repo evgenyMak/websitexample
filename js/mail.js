43b27cb21c951ac52b3a67df2aa56609d2c9c1d3
EvgenyMak <evgenyzheka282662@gmaul.com>
2026-03-29 19:36:14 +0700

    cxxs

--- js/mail.js
+++ js/mail.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#form");
-const scriptUrl = "https://script.google.com/macros/s/AKfycbxjk_y12et_RaASCWDdGuSYmcATC4ih5sB9w6WGk8qGjaH27ALfUFoqjgyae0SEVeYARw/exec";
+const scriptUrl =
+  "https://script.google.com/macros/s/AKfycbxjk_y12et_RaASCWDdGuSYmcATC4ih5sB9w6WGk8qGjaH27ALfUFoqjgyae0SEVeYARw/exec";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,7 +13,20 @@ form.addEventListener("submit", (e) => {
     mode: "no-cors",
   })
     .then(() => {
-      window.location.href = "/index.html?sent=1";
+      // window.location.href = "/index.html?sent=1";
+      document.querySelector(".prev__container").classList.add("visib");
+      [...document.querySelector(".form").querySelectorAll("input")].forEach(
+        (e) => {
+          e.disabled = true;
+        },
+      );
+      document.querySelector(".prev__link").classList.add("hidden");
+      document.querySelector(".form__button").classList.add("hidden");
+
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     })
     .catch(() => {
       alertText(
